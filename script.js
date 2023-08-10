96ca45f69b5c9935a0644f91ddf67b2d984eb942
Felipe Muchulski de Andrades <83315471+felipemuchulski@users.noreply.github.com>
2023-08-10 19:22:42 -0300

    Terminado desáfio

--- script.js
+++ script.js
@@ -16,65 +16,90 @@ let students = [];
 let menuOptions;
 
 while (menuOptions != 4) {
-    menuOptions = Number(prompt(`Bem vindo ao portal da escola! Digite o número desejado para acessar sua opção:
+  menuOptions = Number(
+    prompt(`Bem vindo ao portal da escola! Digite o número desejado para acessar sua opção:
     
     1° Para cadastrar um estudante;
     2° Para calcular a média do estudante;
-    3° Informações gerais dos alunos;
+    3° Informações gerais dos alunos (incluindo se passou ou não);
     4° Para encerrar.    
-    `))
+    `)
+  );
 
-    switch(menuOptions) {
-        case 1: 
-         let name = prompt("Digite o nome do estudante: ");
-         let n1 = Number(prompt("Digite a primeira nota do aluno: "));
-         let n2 = Number(prompt("Digite a segunda nota do aluno:"));
+  switch (menuOptions) {
+    case 1:
+      let nome = prompt("Digite o nome do estudante: ");
+      let firstNote = Number(prompt("Digite a primeira nota do estudante: "));
+      let secondNote = Number(prompt("Digite a segunda nota do estudante: "));
+      let thirdNote = Number(prompt("Digite a terceira nota do estudante: "));
 
-         students.push[{
-            nome: name,
-            firstNote: n1,
-            secondNote: n2
-         }
-        ]
-        alert(`Aluno ${name}, cadastrado com sucesso`);
-        break;
+      students.push({
+        nome: nome,
+        firstNote: firstNote,
+        secondNote: secondNote,
+        thirdNote: thirdNote,
+      });
+      alert(`Estudante ${nome} cadastrado com sucesso`);
+      break;
 
-        case 2: 
-        if(students.length == 0) {
-            alert("Não é possível calcular média pois não existe cadastro")
-        } else {
-            function calculeAvarege (n1, n2){
-            return (n1 + n2) / 2 .toFixed(1) 
-            }
+    case 2:
+      if (students.length == 0) {
+        alert("Não existe cadastro");
+      } else {
+        let avaregeStudents = [];
+        function calculateAvarege(firstNote, secondNote, thirdNote) {
+          let avarege = (firstNote + secondNote + thirdNote) / (2).toFixed(1);
+          return avarege;
+        }
 
-            function printAvarege (student){
-                return `O estudante ${student.nome} tem a média
-                ${calculeAvarege(student.firstNote, student.secondNote)}`; 
-            }
+        for (let student of students) {
+          let avaregeIndividual = calculateAvarege(
+            student.firstNote,
+            student.secondNote,
+            student.thirdNote
+          );
 
-            for(let avarege of students){
-                let avaregeMessage = printAvarege(avarege)
-                alert(avaregeMessage);
-            }
-        }     
-        break;
+          avaregeStudents.push(
+            `A média do estudante ${student.nome} é ${avaregeIndividual}`
+          );
+          alert(avaregeStudents);
+        }
+      }
+
+      break;
+
+    case 3:
+     let studentsInfo = [];
+      if (students.length == 0) {
+        alert("Não é possível informar as informações sem dados");
+      } else {
+        for (let infoStudent of students) {
+          let result = calculateAvarege(
+            infoStudent.firstNote,
+            infoStudent.secondNote,
+            infoStudent.thirdNote
+          );
+
+          let approvedOrNot = result < 7 ? "Reprovado" : "Aprovado";
+          studentsInfo.push(`O estudante ${infoStudent.nome}, teve as notas:
+                Primeira: ${infoStudent.firstNote}
+                Segunda:  ${infoStudent.secondNote}
+                Terceira:  ${infoStudent.thirdNote}
+                
+                Ele está ${approvedOrNot}!`);
+            console.log("to aqui")
+            alert(studentsInfo)
+            console.log("passei o alerta")
         
-        case 3: 
-        if(students.length == 0) {
-            alert("Não existe estudantes cadastrados");
-        } else {
-             let studentsInfo = [];
-            for(let student of students){
-            studentsInfo.push(`O nome do estudante é ${student.nome}, a primeira nota é ${student.firstNote}, a segunda nota é ${student.secondNote} e a média dele é ${calculeAvarege(student.firstNote, student.secondNote)}`);
         }
-        }
-       break;
+      }
+      break;
 
-       case 4: 
-       alert("Programa encerrado");
-       break;
+    case 4:
+      alert("Programa encerrado");
+      break;
 
-       default:
-        alert("Alternativa inválida, tente novamente")
-    }
+    default:
+      alert("Nenhuma das alternativas");
+  }
 }
